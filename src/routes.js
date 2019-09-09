3076b6ba1ff7f3c760b05f85a4428efc9c8bd245
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-09-09 20:25:30 +0100

    header complete

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,24 @@
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import React from 'react';
+import {Image} from 'react-native';
+import {createAppContainer, createStackNavigator} from 'react-navigation';
 
 import Feed from './pages/Feed';
 
-const Routes = createAppContainer(createStackNavigator({Feed}));
+import logo from './assets/instagram.png';
 
-export default Routes;
+export default createAppContainer(
+  createStackNavigator(
+    {
+      Feed,
+    },
+    {
+      headerLayoutPreset: 'center',
+      defaultNavigationOptions: {
+        headerTitle: <Image source={logo} />,
+        headerStyle: {
+          backgroundColor: '#f5f5f5',
+        },
+      },
+    },
+  ),
+);

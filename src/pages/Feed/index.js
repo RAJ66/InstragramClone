10c4d8289da22c2703f4b8489ecc874f96af4d96
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-09-11 16:07:32 +0100

    load more pages

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -5,16 +5,19 @@ import { Post, Header, Avatar, Name, PostImage, Description } from "./styles";
 
 export default function Feed() {
   const [feed, setFeed] = useState([]);
+  const [page, setPage] = useState(1);
+
+  async function loadPage(pageNumber = page) {
+    const response = await fetch(
+      `http://localhost:3000/feed?_expand=author&_limit=5&_page=${pageNumber}`
+    );
+    const data = await response.json();
+    setFeed(data);
+    setPage(pageNumber + 1);
+  }
 
   useEffect(() => {
-    async function loadFeed() {
-      const response = await fetch(
-        "http://localhost:3000/feed?_expand=author&_limit=5&_page=1"
-      );
-      const data = await response.json();
-      setFeed(data);
-    }
-    loadFeed();
+    loadPage();
   }, []);
 
   return (
@@ -22,6 +25,8 @@ export default function Feed() {
       <FlatList
         data={feed}
         keyExtractor={post => String(post.id)}
+        onEndReached={() => loadPage()}
+        onEndReachedThreshold={0.1}
         renderItem={({ item }) => (
           <Post>
             <Header>

48b44302088d65f96c3d96d8467bd9138ad6deee
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-09-11 16:16:54 +0100

    limit page

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -6,12 +6,18 @@ import { Post, Header, Avatar, Name, PostImage, Description } from "./styles";
 export default function Feed() {
   const [feed, setFeed] = useState([]);
   const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
 
   async function loadPage(pageNumber = page) {
+    if (total && pageNumber > total) return;
+    console.log(pageNumber);
+
     const response = await fetch(
       `http://localhost:3000/feed?_expand=author&_limit=5&_page=${pageNumber}`
     );
     const data = await response.json();
+    const totalItem = response.headers.get("X-Total-Count");
+    setTotal(Math.floor(totalItem / 5));
     setFeed([...feed, ...data]);
     setPage(pageNumber + 1);
   }

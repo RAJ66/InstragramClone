dd8f8d2ee4712051798e4d33ee9648f974ffbceb
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-09-20 15:10:48 +0100

    add blur

--- src/components/LazyImage/index.js
+++ src/components/LazyImage/index.js
@@ -4,6 +4,11 @@ import { Small } from "./styles";
 
 export default function LazyImage({ smallSource, source, aspectRatio }) {
   return (
-    <Small source={smallSource} ratio={aspectRatio} resizeMode="contain" />
+    <Small
+      source={smallSource}
+      ratio={aspectRatio}
+      resizeMode="contain"
+      blurRadius={2}
+    />
   );
 }

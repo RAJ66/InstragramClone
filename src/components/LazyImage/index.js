0527dbe4b7ed6220e517ae33471a745bb36cc022
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-09-20 15:24:31 +0100

    image transaction

--- src/components/LazyImage/index.js
+++ src/components/LazyImage/index.js
@@ -1,14 +1,44 @@
-import React from "react";
-
-import { Small } from "./styles";
+import React, { useState, useEffect } from "react";
+import { Animated } from "react-native";
+import { Small, Original } from "./styles";
+import { styled } from "styled-components/native";
 
 export default function LazyImage({ smallSource, source, aspectRatio }) {
+  const opacity = new Animated.Value(0);
+  const [loaded, setLoaded] = useState(false);
+
+  const OriginalAnimated = Animated.createAnimatedComponent(Original);
+
+  function handleAnimate() {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true
+    }).start();
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 1000);
+  }, []);
+
   return (
     <Small
       source={smallSource}
       ratio={aspectRatio}
       resizeMode="contain"
       blurRadius={2}
-    />
+    >
+      {loaded && (
+        <OriginalAnimated
+          styled={{ opacity }}
+          source={source}
+          ratio={aspectRatio}
+          resizeMode="contain"
+          onLoadEnd={handleAnimate}
+        />
+      )}
+    </Small>
   );
 }
